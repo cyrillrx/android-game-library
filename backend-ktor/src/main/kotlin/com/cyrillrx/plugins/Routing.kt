8b381f492d2480af194fde81796c94bded448ba6
Cyril Leroux <cyril0leroux@gmail.com>
2022-05-20 02:49:03 +0200

    ktor-backend: Add HTML DSL

--- backend-ktor/src/main/kotlin/com/cyrillrx/plugins/Routing.kt
+++ backend-ktor/src/main/kotlin/com/cyrillrx/plugins/Routing.kt
@@ -1,15 +1,44 @@
 package com.cyrillrx.plugins
 
 import com.cyrillrx.routes.apiRoute
+import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
+import io.ktor.server.html.*
 import io.ktor.server.routing.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val name = "Welcome to Game Library API!"
+            call.respondHtml(HttpStatusCode.OK) {
+                head {
+                    title {
+                        +name
+                    }
+                }
+                body {
+                    h1 { +name }
+                    h2 { +"Here are some available endpoints" }
+                    ul {
+                        li {
+                            a {
+                                href = "http://localhost:3000/api/v1/games"
+                                +"GET http://localhost:3000/api/v1/games"
+                            }
+                        }
+                        li {
+                            a {
+                                href = "http://localhost:3000/api/v1/game/1"
+                                +"GET http://localhost:3000/api/v1/game/{id}"
+                            }
+                        }
+                        li { +"POST http://localhost:3000/api/v1/game" }
+                        li { +"DELETE http://localhost:3000/api/v1/game/{id}" }
+                    }
+                }
+            }
         }
 
         apiRoute()
